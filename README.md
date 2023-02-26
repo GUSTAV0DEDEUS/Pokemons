92f1bdadc090296fc76f662c48d3af2fc17272a7
GUSTAV0DEDEUS <88512041+GUSTAV0DEDEUS@users.noreply.github.com>
2023-02-26 16:54:18 -0300

    :tada: finish

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1 align="center"> Pokémons </h1>
+<img src="assets/img/gif.gif" width="100%">
+
+# Projeto
+Utilizar dos dados da pokeapi para criar uma lista de pokémons.
+# Link
+> https://gustav0dedeus.github.io/Pokemons/
+# Linguagens
+ * <h2>HTML</h2>
+ * <h2>CSS</h2>
+ * <h2>JS</h2>
---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+
+var quantidade = document.getElementById('quantidade')
+quantidade.addEventListener('keyup',()=>{
+    pegaPokemons(quantidade.value)
+})
+
+pegaPokemons(9)
+function pegaPokemons(quantidade){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit='+quantidade)
+    .then(response => response.json())
+    .then(allpokemon => {
+        pokemon = [];
+
+        allpokemon.results.map((val)=>{
+            
+
+            fetch(val.url)
+            .then(response => response.json())
+            .then(pokemonSingle => {
+                pokemon.push({nome: val.name, imagem: pokemonSingle.sprites.front_default})
+
+                if(pokemon.length == quantidade){
+                    //Finalizamos nossas requisições
+
+                    let poke_boxes = document.querySelector('.pokemon-boxes')
+                    poke_boxes.innerHTML="";
+
+                    pokemon.map((val)=>{
+                        poke_boxes.innerHTML+=`
+                            <div class="pokemon-box">
+                            <img src="`+val.imagem+`">
+                            <div class="box-text">
+                            <p>`+val.nome+`</p>
+                            </div>
+                            </div>
+                        `
+                        /*
+                        <div class="pokemon-box">
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png">
+                        <p>Ditto</p>
+                        </div>
+                        */
+                    })
+                }
+
+            })
+        })
+
+        pokemon.map((val)=>{
+            console.log(val.nme)
+        })
+    })
+}
